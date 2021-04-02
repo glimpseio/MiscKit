77d49a489e2774e8ee43cccf3156c4e9bd4a3923
marcprux <marc@glimpse.io>
2021-04-02 07:36:19 -0400

    feat: XMLTree

---
+++ Sources/MiscKit/XMLTree.swift
@@ -0,0 +1,314 @@
+// Various misc utilities
+// Marc Prud'hommeaux, 2014-2021
+
+import Foundation
+#if canImport(FoundationXML)
+import FoundationXML
+#endif
+
+/// An XML Element Tree.
+@available(macOS 10.14, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public struct XMLTree : Hashable {
+    public struct Entity : OptionSet {
+        /// The format's default value.
+        public let rawValue: UInt
+
+        /// Creates an Entity value with the given raw value.
+        public init(rawValue: UInt) {
+            self.rawValue = rawValue
+        }
+
+        public static var lt: Entity { return Entity(rawValue: 1 << 0) }
+        public static var amp: Entity { return Entity(rawValue: 1 << 1) }
+        public static var gt: Entity { return Entity(rawValue: 1 << 2) }
+        public static var quot: Entity { return Entity(rawValue: 1 << 3) }
+        public static var apos: Entity { return Entity(rawValue: 1 << 4) }
+    }
+
+    /// A single XML document
+    public static let document = XMLTree(elementName: "")
+
+    public var elementName: String
+    public var attributes: [String : String]
+    public var children: [Child]
+    public var namespaceURI: String?
+    public var qualifiedName: String?
+
+    /// This is the document root, which is the only one that permits an empty element name
+    public var isDocument: Bool { return elementName == "" }
+
+    /// Returns all the children of this tree that are element nodes
+    public var elementChildren: [XMLTree] {
+        return children.compactMap { child in
+            if case .element(let element) = child { return element }
+            return nil
+        }
+    }
+
+    /// Returns all the elements in a flattened list
+    public var flattenedElements: [XMLTree] {
+        return self.elementChildren + self.elementChildren.map(\.flattenedElements).joined()
+    }
+
+    /// The attributes for this element
+    public subscript(attribute name: String) -> String? {
+        get { return attributes[name] }
+        set { attributes[name] = newValue }
+    }
+
+    /// A `Child` consists of all the data strucutres that may be contained within an XML element.
+    public enum Child : Hashable {
+        case element(XMLTree)
+        case content(String)
+        case comment(String)
+        case cdata(Data)
+        case whitespace(String)
+        case processingInstruction(target: String, data: String?)
+    }
+
+    public init(elementName: String, attributes: [String : String] = [:], children: [Child] = [], namespaceURI: String? = nil, qualifiedName: String? = nil) {
+        self.elementName = elementName
+        self.attributes = attributes
+        self.children = children
+        self.namespaceURI = namespaceURI
+        self.qualifiedName = qualifiedName
+    }
+
+    /// Appends the given tree as an element child
+    public mutating func append(_ element: XMLTree) {
+        self.children.append(.element(element))
+    }
+
+    /// Adds the given element to the node.
+    /// - Parameters:
+    ///   - elementName: the name of the element
+    ///   - attributes: any attributes for the element
+    ///   - content: the textual content of the element
+    ///   - CDATA: whether the text content should be in a CDATA tag (default: false)
+    /// - Returns: the appended XMLTree
+    @discardableResult public mutating func addElement(_ elementName: String, attributes: [String: String] = [:] , content: String? = nil, CDATA: Bool = false) -> XMLTree {
+        var node = XMLTree(elementName: elementName, attributes: attributes)
+        if let content = content {
+            if CDATA {
+                node.children.append(.cdata(content.data(using: .utf8) ?? .init()))
+            } else {
+                node.children.append(.content(content))
+            }
+        }
+        self.children.append(.element(node))
+        return self
+    }
+
+    public func xmlString(declaration: String = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>", quote: String = "\"", compactCloseTags: Bool = false, escape entities: Entity = [.lt, .amp, .gt], commentScriptCDATA: Bool = false, attributeSorter: ([String: String]) -> [(String, String)] = { Array($0).sorted(by: { $0.0 < $1.0 }) }) -> String {
+        var str = ""
+
+        // when we use single quotes for entites, we escape them; same for double-quotes
+        var entities = entities
+        entities.insert(quote == "\"" ? .quot : .apos)
+
+        if isDocument {
+            str += declaration // the document header is the XML declaration
+        } else {
+            str += "<" + elementName
+            for (key, value) in attributeSorter(attributes) {
+                str += " " + key + "=" + quote + value.escapedXMLEntities(entities) + quote
+            }
+            if children.isEmpty && compactCloseTags {
+                str += "/"
+            }
+            str += ">"
+        }
+
+        for child in children {
+            switch child {
+            case .element(let element):
+                str += element.xmlString(quote: quote, compactCloseTags: compactCloseTags, escape: entities, commentScriptCDATA: commentScriptCDATA, attributeSorter: attributeSorter)
+            case .content(let content):
+                str.append(content.escapedXMLEntities(entities))
+            case .comment(let comment):
+                str += "<!--" + comment + "-->"
+            case .cdata(let data):
+                // note that we manually replace "]]>" with "]] >" in order to prevent it from breaking the CDATA
+                // this is potentially dangerous, because the code might contains "]]>" that runs in a meaningful way.
+                let code = (String(data: data, encoding: .utf8)?.replacingOccurrences(of: "]]>", with: "]] >") ?? "")
+                //dbg("CDATA", data.localizedByteCount, elementName)
+                if commentScriptCDATA && elementName == "script" {
+                    // https://www.w3.org/TR/html-polyglot/#dfn-safe-text-content
+                    str += "//<![CDATA[\n" + code + "\n//]]>"
+                } else {
+                    str += "<![CDATA[" + code + "]]>"
+                }
+            case .whitespace(let whitespace):
+                str += whitespace
+            case .processingInstruction(let target, let data):
+                str += "<?" + target
+                if let data = data {
+                    str += " " + data
+                }
+                str += "?>"
+            }
+        }
+
+        if !isDocument && !(children.isEmpty && compactCloseTags) {
+            str += "</" + elementName + ">"
+        }
+
+        return str
+    }
+
+    public static func parse(data: Data) throws -> XMLTree {
+        let parser = XMLParser(data: data)
+        let delegate = Delegate()
+        parser.delegate = delegate
+        if parser.parse() == false {
+            if let error = parser.parserError {
+                // try data.write(to: URL(fileURLWithPath: NSTemporaryDirectory() + "/error.xml"))
+                // dbg("parser error line", parser.lineNumber, "column", parser.columnNumber, "error", error as NSError)
+                throw error // TODO: include lineNumber & columnNumber in the error
+            } else if let parseError = delegate.parseErrors.first {
+                throw parseError
+            } else if let validationError = delegate.validationErrors.first {
+                throw validationError
+            } else {
+                throw err(loc("Unable to parse XML document"))
+            }
+        }
+
+        if delegate.elements.count != 1 {
+            throw err(locfmt("Bad element count %d", delegate.elements.count))
+        }
+
+        return delegate.currentElement
+    }
+
+    private class Delegate : NSObject, XMLParserDelegate {
+        var elements: [XMLTree] = []
+        var parseErrors: [Error] = []
+        var validationErrors: [Error] = []
+
+        /// Convenience getter/setter for the button of the elements stack
+        var currentElement: XMLTree {
+            get {
+                return elements.last!
+            }
+
+            set {
+                if elements.isEmpty {
+                    elements = [newValue]
+                } else {
+                    elements[elements.count-1] = newValue
+                }
+            }
+        }
+
+        func parserDidStartDocument(_ parser: XMLParser) {
+            // the root document is simply an empty element name
+            elements.append(XMLTree(elementName: ""))
+        }
+
+        func parserDidEndDocument(_ parser: XMLParser) {
+            // we do nothing her because we hold on to the root document
+        }
+
+        func parser(_ parser: XMLParser, foundNotationDeclarationWithName name: String, publicID: String?, systemID: String?) {
+        }
+
+
+        func parser(_ parser: XMLParser, foundUnparsedEntityDeclarationWithName name: String, publicID: String?, systemID: String?, notationName: String?) {
+        }
+
+        func parser(_ parser: XMLParser, foundAttributeDeclarationWithName attributeName: String, forElement elementName: String, type: String?, defaultValue: String?) {
+            dbg("foundAttributeDeclarationWithName", attributeName, elementName, type, defaultValue)
+        }
+
+        func parser(_ parser: XMLParser, foundElementDeclarationWithName elementName: String, model: String) {
+            dbg("foundElementDeclarationWithName", elementName, model)
+        }
+
+        func parser(_ parser: XMLParser, foundInternalEntityDeclarationWithName name: String, value: String?) {
+        }
+
+        func parser(_ parser: XMLParser, foundExternalEntityDeclarationWithName name: String, publicID: String?, systemID: String?) {
+        }
+
+        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+            elements.append(XMLTree(elementName: elementName, attributes: attributeDict, children: [], namespaceURI: namespaceURI, qualifiedName: qName))
+        }
+
+        func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            if let element = elements.popLast() { // remove the last element…
+                currentElement.children.append(.element(element)) // … and add it as a child to the parent
+            }
+        }
+
+        func parser(_ parser: XMLParser, foundCharacters string: String) {
+            currentElement.children.append(.content(string))
+        }
+
+        func parser(_ parser: XMLParser, foundIgnorableWhitespace whitespaceString: String) {
+            currentElement.children.append(.whitespace(whitespaceString))
+        }
+
+        func parser(_ parser: XMLParser, foundProcessingInstructionWithTarget target: String, data: String?) {
+            currentElement.children.append(.processingInstruction(target: target, data: data))
+        }
+
+        func parser(_ parser: XMLParser, foundComment comment: String) {
+            currentElement.children.append(.comment(comment))
+        }
+
+        func parser(_ parser: XMLParser, foundCDATA CDATABlock: Data) {
+            currentElement.children.append(.cdata(CDATABlock))
+        }
+
+        func parser(_ parser: XMLParser, resolveExternalEntityName name: String, systemID: String?) -> Data? {
+            return nil
+        }
+
+        func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+            parseErrors.append(parseError)
+        }
+
+        func parser(_ parser: XMLParser, validationErrorOccurred validationError: Error) {
+            validationErrors.append(validationError)
+        }
+    }
+
+//    public struct ParseError : Error, Hashable {
+//        /// The line number in the original document at which the error occured
+//        public var lineNumber: Int
+//        /// The column number in the original document at which the error occured
+//        public var columnNumber: Int
+//        /// The underlying error code for the error
+//        public var code: XMLParser.ErrorCode
+//        /// Whether this is a validation error or a parser error
+//        public var validation: Bool
+//    }
+}
+
+@available(macOS 10.14, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+private extension String {
+
+    /// Returns the string with the given XML entites escaped; the default does not include single apostrophes
+    func escapedXMLEntities(_ entities: XMLTree.Entity) -> String {
+        var str = ""
+        str.reserveCapacity(self.count)
+        let lt = entities.contains(.lt)
+        let amp = entities.contains(.amp)
+        let gt = entities.contains(.gt)
+        let quot = entities.contains(.quot)
+        let apos = entities.contains(.apos)
+        for char in self {
+            switch char {
+            case "<" where lt: str.append("&lt;")
+            case "&" where amp: str.append("&amp;")
+            case ">" where gt: str.append("&gt;")
+            case "\"" where quot: str.append("&quot;")
+            case "'" where apos: str.append("&apos;") // messes up CSS, and isn't necessary
+            default: str.append(char)
+            }
+        }
+        return str
+    }
+}
+
--- Tests/MiscKitTests/MiscKitTests.swift
+++ Tests/MiscKitTests/MiscKitTests.swift
@@ -46,6 +46,83 @@ class MiscKitTests : XCTestCase {
         
         XCTAssertEqual("XYZ", str)
     }
+
+    func testParseXML() throws {
+        func roundTrip(xml string: String, to result: String? = nil, quote: String = "\"", compactCloseTags: Bool = false, line: UInt = #line) throws {
+            let item = try XMLTree.parse(data: string.data(using: .utf8) ?? .init())
+            let xmlString = item.xmlString(declaration: "", quote: quote, compactCloseTags: compactCloseTags)
+            XCTAssertEqual(result ?? string, xmlString, line: line)
+        }
+
+        try roundTrip(xml: "<x y='123'>z</x>", quote: "'")
+        try roundTrip(xml: "<x y='ABC'> z </x>", quote: "'")
+        try roundTrip(xml: "<x y='AB\\C'> z </x>", quote: "'")
+        try roundTrip(xml: "<x y='123'> z <q:a><![CDATA[111]]><r><s></s><!-- COMMENT --></r></q:a> </x>", quote: "'")
+        try roundTrip(xml: "<俄语 լեզու='ռուսերեն'>данные</俄语>", quote: "'")
+
+        // https://www.w3.org/XML/Test/
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/
+
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/001.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/002.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc ></doc>", to: "<doc></doc>") // whitespace fidelity in tag not supported
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/008.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc>&amp;&lt;&gt;&quot;'</doc>", quote: "\"")
+        try roundTrip(xml: "<doc>&amp;&lt;&gt;\"&apos;</doc>", quote: "'")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/093.xml?rev=1.2
+        try roundTrip(xml: "<doc>\n\n</doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/017.xml?rev=1.2
+        try roundTrip(xml: "<doc><?pi some data ?><?x?></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/009.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc>&#x20;</doc>", to: "<doc> </doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/010.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc a1=\"v1\" ></doc>", to: "<doc a1=\"v1\"></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/011.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc a1=\"v1\" a2=\"v2\"></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/012.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc :=\"v1\"></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/013.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc _.-0123456789=\"v1\"></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/014.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc abcdefghijklmnopqrstuvwxyz=\"v1\"></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/016.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><?pi?></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/018.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><![CDATA[<foo>]]></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/019.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><![CDATA[<&]]></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/020.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><![CDATA[<&]>]]]></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/021.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><!-- a comment --></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/022.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><!-- a comment ->--></doc>")
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/025.xml?rev=1.1.1.1
+        try roundTrip(xml: "<doc><foo/><foo><bar/></foo></doc>", compactCloseTags: true)
+
+        // https://dev.w3.org/cvsweb/2001/XML-Test-Suite/xmlconf/xmltest/valid/sa/043.xml?rev=1.1.1.1
+        //try roundTrip(xml: "<doc a1=\"foo\nbar\"></doc>")
+    }
+
 }
 #endif
 
