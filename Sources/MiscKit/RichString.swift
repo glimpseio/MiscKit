8484994be9ec2600ae066f3d7b5ef3e7c06583be
marcprux <marc@glimpse.io>
2021-06-11 14:01:45 -0400

    feat: RichString stand-in for AttributedString

--- Sources/MiscKit/RichString.swift
+++ Sources/MiscKit/RichString.swift
@@ -1,69 +1,70 @@
+/// RichString  is a partial back-port of `Foundation.AttributedString` for pre-5.5 releases.
 
 import Foundation
 
 
-//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-//public protocol AttributedStringAttributeMutation {
-//
-//    mutating func setAttributes(_ attributes: AttributeContainer)
-//
-//    mutating func mergeAttributes(_ attributes: AttributeContainer)
-//
-//    mutating func replaceAttributes(_ attributes: AttributeContainer, with others: AttributeContainer)
-//}
-//
-//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-//public protocol AttributedStringKey {
-//
-//    associatedtype Value : Hashable
-//
-//    static var name: String { get }
-//}
-//
-//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-//extension AttributedStringKey {
-//
-//    public var description: String { get }
-//}
-//
-//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-//public protocol AttributedStringProtocol : AttributedStringAttributeMutation, CustomStringConvertible, Hashable {
-//
-//    var startIndex: AttributedString.Index { get }
-//
-//    var endIndex: AttributedString.Index { get }
-//
-//    var runs: AttributedString.Runs { get }
-//
-//    var characters: AttributedString.CharacterView { get }
-//
-//    var unicodeScalars: AttributedString.UnicodeScalarView { get }
-//}
-//
-//
-//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-//@dynamicMemberLookup public struct AttributedSubstring {
-//    public init()
-//    public var base: AttributedString { get }
-//    public var description: String { get }
-//}
+#if canImport(TabularData) // as a proxy for @available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public typealias RichStringBase = Foundation.AttributedStringProtocol
+#else
+public typealias RichStringBase = CustomStringConvertible & Hashable
+#endif
 
+/// A string with formatting.
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public protocol RichStringProtocol : RichStringBase {
+    associatedtype Index
+    associatedtype UnicodeScalarView
 
+    var startIndex: Index { get }
+    var endIndex: Index { get }
+    var unicodeScalars: UnicodeScalarView { get }
+}
 
+#if canImport(TabularData)
+/// A rich string.
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public typealias RichString = Foundation.AttributedString
 
-//#if true
-//public protocol RichStringProtocol : AttributedStringProtocol {
-//}
-//#else
-//public protocol RichStringProtocol : AttributedStringProtocol {
-//}
-//#endif
-//
-//public struct RichString : RichStringProtocol {
-//
-//}
-//
-//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-//public extension Foundation.AttributedString : RichStringProtocol {
-//
-//}
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+extension RichString : RichStringProtocol {
+    // conforms via `Foundation.AttributedString`
+}
+#else
+
+/// Stopgap implementation of `AttributedString`
+///
+@available(*, deprecated, renamed: "RichString")
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public typealias AttributedString = RichString
+
+/// A rich string.
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public typealias RichString = RawRichString
+
+/// A type that confroms to `AttributedStringProtocol` with raw text.
+/// Formatting is not supported.
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+public struct RawRichString: RichStringProtocol, RawRepresentable {
+    public let rawValue: String
+
+    public init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+
+    public typealias Storage = Swift.String
+    public typealias Index = Storage.Index
+    public typealias UnicodeScalarView = Storage.UnicodeScalarView
+
+    public var startIndex: AttributedString.Index { rawValue.startIndex }
+    public var endIndex: AttributedString.Index { rawValue.endIndex }
+    public var unicodeScalars: AttributedString.UnicodeScalarView { rawValue.unicodeScalars }
+
+    public var description: String { rawValue }
+}
+
+#endif
