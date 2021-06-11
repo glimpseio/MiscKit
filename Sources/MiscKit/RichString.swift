6de298173e63ebe680f6c58118ed6c944eca38fb
marcprux <marc@glimpse.io>
2021-06-11 10:30:58 -0400

    chore: checkpoint

---
+++ Sources/MiscKit/RichString.swift
@@ -0,0 +1,69 @@
+
+import Foundation
+
+
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+//public protocol AttributedStringAttributeMutation {
+//
+//    mutating func setAttributes(_ attributes: AttributeContainer)
+//
+//    mutating func mergeAttributes(_ attributes: AttributeContainer)
+//
+//    mutating func replaceAttributes(_ attributes: AttributeContainer, with others: AttributeContainer)
+//}
+//
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+//public protocol AttributedStringKey {
+//
+//    associatedtype Value : Hashable
+//
+//    static var name: String { get }
+//}
+//
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+//extension AttributedStringKey {
+//
+//    public var description: String { get }
+//}
+//
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+//public protocol AttributedStringProtocol : AttributedStringAttributeMutation, CustomStringConvertible, Hashable {
+//
+//    var startIndex: AttributedString.Index { get }
+//
+//    var endIndex: AttributedString.Index { get }
+//
+//    var runs: AttributedString.Runs { get }
+//
+//    var characters: AttributedString.CharacterView { get }
+//
+//    var unicodeScalars: AttributedString.UnicodeScalarView { get }
+//}
+//
+//
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+//@dynamicMemberLookup public struct AttributedSubstring {
+//    public init()
+//    public var base: AttributedString { get }
+//    public var description: String { get }
+//}
+
+
+
+
+//#if true
+//public protocol RichStringProtocol : AttributedStringProtocol {
+//}
+//#else
+//public protocol RichStringProtocol : AttributedStringProtocol {
+//}
+//#endif
+//
+//public struct RichString : RichStringProtocol {
+//
+//}
+//
+//@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+//public extension Foundation.AttributedString : RichStringProtocol {
+//
+//}
